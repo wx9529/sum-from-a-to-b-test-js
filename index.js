6beb9e42d351c40669ad2b5672b817929c813bf0
wangxin <xinwang213z@gmail.com>
2022-03-12 04:53:55 +0000

    sum-a-to-b

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN < toN) {
+    return fromN + sum(fromN + 1, toN);
+  }
+  return toN;
 }
 
 module.exports = sum;
